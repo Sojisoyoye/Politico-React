8c7c7fd9619f0436ddc7ea13f69d809981ea6845
Soji Soyoye <soji.soyoye@gmail.com>
2019-06-25 20:22:20 +0100

    set up home page for app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const App = () => {
-    return <div>Welcome to Politico-React</div>
-};
+// import './index.css';
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
